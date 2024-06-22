59031d2944fd0cc0c2fe8a61c4eeac7759c504f9
Rajendra Singh Arora <rajesharora77077@gmail.com>
2024-06-22 20:15:50 +0530

    add student page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/Config/theme.dart';
-import 'package:myapp/Pages/Leave/LeaveForm.dart';
-import 'package:myapp/Pages/Notification/Notification.dart';
-import 'package:myapp/Pages/Student_profile/StudentProfile.dart';
 import 'package:myapp/Widgets/splash_screen.dart';
-
-import 'Pages/DashBoard/dashboard.dart';
-import 'Pages/DashBoard/dashboardRoutes/LeaveForm.dart';
-import 'Pages/DashBoard/dashboardRoutes/Notification.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       themeMode: ThemeMode.light,
-      home: NotificationPage(),
+      home: const SplashScreen(),
     );
   }
 }
