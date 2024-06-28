d712e6ec4766842f3861310014245fe767b8c598
Rajendra Singh Arora <rajesharora77077@gmail.com>
2024-06-28 17:55:00 +0530

    added border and background svg)

--- lib/Pages/TeacherPages/AttendanceMark/attendance.dart
+++ lib/Pages/TeacherPages/AttendanceMark/attendance.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'edit_attendance.dart';
 import 'mark_attendance.dart';
 import 'package:myapp/Config/colors.dart'; // Make sure to import your colors.dart file if you are using custom colors
@@ -10,46 +11,57 @@ class AttendancePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: const Text('Attendance')),
+        title: const Center(child: Text('Attendance')),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
+      body: Stack(
         children: [
-          AttendanceCard(
-            subject: 'Mathematics',
-            time: '08:00am - 08:40am',
-            classInfo: 'Class: 3A',
-            period: 'Period 1',
+          // Background SVG
+          Positioned.fill(
+            child: SvgPicture.asset(
+              'assets/icons/bgDesignLayer.svg',
+              fit: BoxFit.cover
+            ),
           ),
-          AttendanceCard(
-            subject: 'Social Science',
-            time: '08:40am - 09:20am',
-            classInfo: 'Class: 4B',
-            period: 'Period 2',
-          ),
-          AttendanceCard(
-            subject: 'Mathematics',
-            time: '09:20am - 10:00am',
-            classInfo: 'Class: 5B',
-            period: 'Period 3',
-          ),
-          AttendanceCard(
-            subject: 'Mathematics',
-            time: '10:00am - 10:40am',
-            classInfo: 'Class: 5A',
-            period: 'Period 4',
-          ),
-          AttendanceCard(
-            subject: 'Social Science',
-            time: '11:20am - 12:00pm',
-            classInfo: 'Class: 3B',
-            period: 'Period 5',
-          ),
-          AttendanceCard(
-            subject: 'Mathematics',
-            time: '12:00pm - 12:40pm',
-            classInfo: 'Class: 6A',
-            period: 'Period 6',
+          ListView(
+            padding: const EdgeInsets.all(8.0),
+            children: [
+              AttendanceCard(
+                subject: 'Mathematics',
+                time: '08:00am - 08:40am',
+                classInfo: 'Class: 3A',
+                period: 'Period 1',
+              ),
+              AttendanceCard(
+                subject: 'Social Science',
+                time: '08:40am - 09:20am',
+                classInfo: 'Class: 4B',
+                period: 'Period 2',
+              ),
+              AttendanceCard(
+                subject: 'Mathematics',
+                time: '09:20am - 10:00am',
+                classInfo: 'Class: 5B',
+                period: 'Period 3',
+              ),
+              AttendanceCard(
+                subject: 'Mathematics',
+                time: '10:00am - 10:40am',
+                classInfo: 'Class: 5A',
+                period: 'Period 4',
+              ),
+              AttendanceCard(
+                subject: 'Social Science',
+                time: '11:20am - 12:00pm',
+                classInfo: 'Class: 3B',
+                period: 'Period 5',
+              ),
+              AttendanceCard(
+                subject: 'Mathematics',
+                time: '12:00pm - 12:40pm',
+                classInfo: 'Class: 6A',
+                period: 'Period 6',
+              ),
+            ],
           ),
         ],
       ),
@@ -74,9 +86,13 @@ class AttendanceCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 6,
+      elevation: 15,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
+        side: const BorderSide(
+          color: Colors.black54, // Add your preferred border color
+          width: 1, // Add your preferred border width
+        ),
       ),
       margin: const EdgeInsets.symmetric(vertical: 10),
       child: Padding(
@@ -105,6 +121,12 @@ class AttendanceCard extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 12),
+            const Divider(
+              color: Colors.black54,
+              height: 1,
+              thickness: 1,
+            ),
+            const SizedBox(height: 12),
             Row(
               children: [
                 Expanded(
@@ -113,13 +135,15 @@ class AttendanceCard extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const MarkAttendancePage()),
+                          builder: (context) => const MarkAttendancePage(),
+                        ),
                       );
                     },
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red.shade700,
-                        textStyle: Theme.of(context).textTheme.labelMedium,
-                        foregroundColor: dOnBackgroundColor),
+                      backgroundColor: Colors.red.shade700,
+                      textStyle: Theme.of(context).textTheme.labelMedium,
+                      foregroundColor: dOnBackgroundColor,
+                    ),
                     child: const Text('Mark Attendance'),
                   ),
                 ),
@@ -130,13 +154,15 @@ class AttendanceCard extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const EditAttendancePage()),
+                          builder: (context) => const EditAttendancePage(),
+                        ),
                       );
                     },
                     style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey.shade600,
-                        textStyle: Theme.of(context).textTheme.labelMedium,
-                        foregroundColor: dOnBackgroundColor),
+                      backgroundColor: Colors.grey.shade600,
+                      textStyle: Theme.of(context).textTheme.labelMedium,
+                      foregroundColor: dOnBackgroundColor,
+                    ),
                     child: const Text('Edit Attendance'),
                   ),
                 ),
