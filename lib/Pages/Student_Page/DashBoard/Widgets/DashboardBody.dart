2adbebe8df5fa97acf4cd443cc9261df9f459632
Ansh Raj <anshraj0078@gmail.com>
2024-07-02 14:17:57 +0530

    added Time Table page

--- lib/Pages/Student_Page/DashBoard/Widgets/DashboardBody.dart
+++ lib/Pages/Student_Page/DashBoard/Widgets/DashboardBody.dart
@@ -12,6 +12,7 @@ import 'package:myapp/Pages/Student_Page/Transport/TransortPage.dart';
 import 'package:myapp/Pages/Student_Page/academy_Calender/AcademyCalender.dart';
 import 'package:myapp/Pages/Student_Page/ask_doubt/askDoubt.dart';
 import 'package:myapp/Pages/Student_Page/official_Detail/OfficialDetail.dart';
+import 'package:myapp/Pages/Student_Page/timeTable/timeTablePage.dart';
 
 class DashboardBody extends StatelessWidget {
   const DashboardBody({super.key});
@@ -112,7 +113,9 @@ class DashboardBody extends StatelessWidget {
                 ),
                 DashboardBox(
                   context,
-                  () {},
+                  () {
+                    Get.to(const TimeTablePage());
+                  },
                   AssetsIamge.dTimeTableSVG,
                   "Time Table",
                 ),
---
+++ lib/Pages/Student_Page/timeTable/timeTablePage.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:myapp/Config/images.dart';
+import 'package:myapp/Pages/Student_Page/timeTable/widgets/timeTableBox.dart';
+
+class TimeTablePage extends StatelessWidget {
+  const TimeTablePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "Time Table",
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+        leading: InkWell(
+          onTap: () {
+            Get.back();
+          },
+          child: const Icon(Icons.arrow_back_ios_new_outlined),
+        ),
+      ),
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            AssetsIamge.bgDesignSVG,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(right: 20, left: 20),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromRGBO(250, 225, 225, 0.6),
+                ),
+                child: const Column(
+                  children: [
+                    TimeTableBox(
+                      subjectName: "Science",
+                      time: "08:15 AM - 08:45 Pm",
+                      teacherName: "Teacher Name",
+                      period: "1",
+                    ),
+                    SizedBox(height: 5),
+                    TimeTableBox(
+                      subjectName: "Science",
+                      time: "08:15 AM - 08:45 Pm",
+                      teacherName: "Teacher Name",
+                      period: "1",
+                    ),
+                    SizedBox(height: 5),
+                    TimeTableBox(
+                      subjectName: "Science",
+                      time: "08:15 AM - 08:45 Pm",
+                      teacherName: "Teacher Name",
+                      period: "1",
+                    ),
+                    SizedBox(height: 5),
+                    TimeTableBox(
+                      subjectName: "Science",
+                      time: "08:15 AM - 08:45 Pm",
+                      teacherName: "Teacher Name",
+                      period: "1",
+                    ),
+                    SizedBox(height: 5),
+                    TimeTableBox(
+                      subjectName: "Science",
+                      time: "08:15 AM - 08:45 Pm",
+                      teacherName: "Teacher Name",
+                      period: "1",
+                    ),
+                    SizedBox(height: 5),
+                    TimeTableBox(
+                      subjectName: "Science",
+                      time: "08:15 AM - 08:45 Pm",
+                      teacherName: "Teacher Name",
+                      period: "1",
+                    ),
+                    SizedBox(height: 5),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
