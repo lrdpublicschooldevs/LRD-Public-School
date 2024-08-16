474f3078a5b9d4a79e674c5d9f5f70cee7eddbc1
Rajendra Singh Arora <rajesharora77077@gmail.com>
2024-08-16 07:51:23 +0530

    Fix: Minor fix on SyllabusBox Screen

--- lib/Pages/Student_Page/Syllabus/widgets/syllabusBox.dart
+++ lib/Pages/Student_Page/Syllabus/widgets/syllabusBox.dart
@@ -18,56 +18,59 @@ class _SyllabusBoxState extends State<SyllabusBox> {
     final bool isDesktop = currentWidth > 1000;
     final bool isTablet = currentWidth > 600;
 
-    return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: isDesktop ? 40 : isTablet ? 30 : 20,
-      ),
-      padding: EdgeInsets.all(isDesktop ? 30 : isTablet ? 25 : 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: const Color.fromRGBO(250, 225, 225, 0.6),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Select Class",
-            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-              fontSize: isDesktop ? 20 : isTablet ? 18 : 16,
-            ),
-          ),
-          const CustomDropdown(items: [
-            {'value': '1', 'label': '1'},
-            {'value': '2', 'label': '2'},
-            {'value': '3', 'label': '3'},
-            {'value': '4', 'label': '4'},
-          ], hint: "Choose Class"),
-          const SizedBox(height: 15),
-          Text(
-            "Select Subjects",
-            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-              fontSize: isDesktop ? 20 : isTablet ? 18 : 16,
-            ),
-          ),
-          const CustomDropdown(items: [
-            {'value': 'science', 'label': 'Science'},
-            {'value': 'english', 'label': 'English'},
-            {'value': 'mathematics', 'label': 'Mathematics'},
-            {'value': 'computer', 'label': 'Computer'},
-          ], hint: "Choose Subject"),
-          const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              PrimaryBtn(
-                btnName: "Preview",
-                ontap: () {
-                  Get.to(() => const SyllabusViewPage());
-                },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: isDesktop ? 40 : isTablet ? 30 : 20,
+        ),
+        padding: EdgeInsets.all(isDesktop ? 30 : isTablet ? 25 : 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: const Color.fromRGBO(250, 225, 225, 0.6),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Select Class",
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontSize: isDesktop ? 20 : isTablet ? 18 : 16,
               ),
-            ],
-          )
-        ],
+            ),
+            const CustomDropdown(items: [
+              {'value': '1', 'label': '1'},
+              {'value': '2', 'label': '2'},
+              {'value': '3', 'label': '3'},
+              {'value': '4', 'label': '4'},
+            ], hint: "Choose Class"),
+            const SizedBox(height: 15),
+            Text(
+              "Select Subjects",
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontSize: isDesktop ? 20 : isTablet ? 18 : 16,
+              ),
+            ),
+            const CustomDropdown(items: [
+              {'value': 'science', 'label': 'Science'},
+              {'value': 'english', 'label': 'English'},
+              {'value': 'mathematics', 'label': 'Mathematics'},
+              {'value': 'computer', 'label': 'Computer'},
+            ], hint: "Choose Subject"),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                PrimaryBtn(
+                  btnName: "Preview",
+                  ontap: () {
+                    Get.to(() => const SyllabusViewPage());
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       themeMode: ThemeMode.light,
-      home: const ReportCardPage(),
+      home: const DashboardPage(),
     );
   }
 }
